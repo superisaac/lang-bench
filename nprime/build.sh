92a4d22e85433fa0ac680eed34040f5439befdd5
Zeng Ke <superisaac.ke@gmail.com>
2019-05-17 18:15:16 +0800

    updated build.sh

--- nprime/build.sh
+++ nprime/build.sh
@@ -8,24 +8,34 @@ g++ nprime.cpp -O3 -o build/nprime-c++-O3
 if [ -n $(which em++) ]; then
     echo em++ nprime.cpp -o build/nprime-wasm
     em++ nprime.cpp -o build/nprime-wasm.js
+else
+    echo emscripten need to be installed
 fi
 
 if [ -n $(which go) ]; then
     echo go build nprime.go -o build/nprime-go
     go build nprime.go -o build/nprime-go
+else
+    echo go need to be installed
 fi
 
 if [ -n $(which go) ]; then
     echo nim c -x:off --opt:speed -d:release nprime.nim
     nim c -x:off --opt:speed -d:release nprime.nim
+else
+    echo nim need to be installed
 fi
 
 if [ -n $(which javac) ]; then
     echo javac -d build nprime.java
     javac -d build nprime.java
+else
+    echo java sdk need to be installed
 fi
 
 if [ -n $(which rustc) ]; then
     echo rustc nprime.rs -O -o build/nprime-rust
     rustc nprime.rs -O -o build/nprime-rust
+else
+    echo rust need to be installed
 fi
